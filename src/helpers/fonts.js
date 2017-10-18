179503bd65d376065c88196f293090a8eefde163
Ximo <ximo.mompo@gmail.com>
2017-10-18 18:52:35 +0200

    Profile view basic

---
+++ src/helpers/fonts.js
@@ -0,0 +1,6 @@
+export const black = 'Roboto-Black';
+export const bold = 'Roboto-Bold';
+export const medium = 'Roboto-Medium';
+export const regular = 'Roboto-Regular';
+export const light = 'Roboto-Light';
+export const thin = 'Roboto-Thin';
--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, Image, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import firebase from 'react-native-firebase';
-import { Button } from '../../components/Commons';
+import { Button, MainView } from '../../components/Commons';
+import * as colors from '../../helpers/colors';
+import * as fonts from '../../helpers/fonts';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
+    name: {
+        fontFamily: fonts.bold,
+        fontSize: 20,
+    },
+    email: {
+        fontFamily: fonts.regular,
+        color: colors.gray,
+    },
+    image: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        borderWidth: 1,
+        borderColor: colors.gray,
+        margin: 20,
+    },
+});
 
 class Profile extends Component {
     onLogout = () => {
@@ -10,16 +34,19 @@ class Profile extends Component {
     }
     render() {
         return (
-            <View>
-                <Text>
-                    Profile
-                </Text>
+            <MainView style={styles.container}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: firebase.auth().currentUser.photoURL }}
+                />
+                <Text style={styles.name}>{firebase.auth().currentUser.displayName}</Text>
+                <Text style={styles.email}>{firebase.auth().currentUser.email}</Text>
                 <Button
                     title="CERRAR SESIÓN"
                     onPress={() => this.onLogout()}
                     fullWidth
                 />
-            </View>
+            </MainView>
         );
     }
 }
